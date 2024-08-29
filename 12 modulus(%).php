51d2166d027795c644f4f634c3725ff7e74955b7
amellyaa12 <lyaaamel4@gmail.com>
2024-08-29 23:44:19 +0700

    12

--- 12 modulus(%).php
+++ 12 modulus(%).php
@@ -1,8 +1,8 @@
 <?php
 $a = 10;
 $b = 3;
+
 $hasil = $a % $b; // Hasilnya adalah 1
 echo "$hasil";
 
 echo "<br><br><strong>By Amellya</strong>";
-
--- 13 Eksponensial.php
+++ 13 Eksponensial.php
@@ -1,9 +1,8 @@
 <?php
 $a = 2;
 $b = 3;
+
 $hasil = $a ** $b;// Hasilnya adalah 8 (2 pangkat 3)
 echo "$hasil";
 
 echo "<br><br><strong>By Amellya</strong>";
-
-
