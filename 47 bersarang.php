0aec3758296377231e8ddb40a880cf0fd7f2c406
amellyaa12 <lyaaamel4@gmail.com>
2024-08-30 09:35:35 +0700

    47 bersarang

---
+++ 47 bersarang.php
@@ -0,0 +1,43 @@
+<?php
+
+$umur = 17;
+$menikah = false;
+
+if($umur > 18){
+    if($menikah){
+        echo "<br>Selamat datang pak!";
+    } else {
+        echo "<br>Selamat datang wahai pemuda!";
+    }
+} else {
+    echo "<br>Maaf website ini hanya untuk yang sudah berumur 18+";
+}
+
+$umur = 25;
+$menikah = false;
+
+if($umur > 18){
+    if($menikah){
+        echo  "<br>Selamat datang pak!";
+    } else {
+        echo "<br>Selamat datang wahai pemuda!";
+    }
+} else {
+    echo "<br>Maaf website ini hanya untuk yang sudah berumur 18+";
+}
+
+$umur = 47;
+$menikah = false;
+
+if($umur > 18){
+    if($menikah){
+        echo "<br>Selamat datang pak!";
+    } else {
+        echo "<br>Selamat datang wahai pemuda!";
+    }
+} else {
+    echo "<br>Maaf website ini hanya untuk yang sudah berumur 18+";
+}
+
+echo "<br><br><strong>By Amellya</strong>";
+?>
